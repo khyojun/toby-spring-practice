f6e3face55ac5223759d41bd9dcd722daec86c29
Khyojun <38220795+khyojun@users.noreply.github.com>
2024-07-03 21:56:01 +0900

    testcode 일부 작성

--- src/main/java/tobyspring/practicetobyspring/Client.java
+++ src/main/java/tobyspring/practicetobyspring/Client.java
@@ -2,7 +2,6 @@ package tobyspring.practicetobyspring;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import tobyspring.practicetobyspring.payment.Payment;
@@ -12,20 +11,12 @@ public class Client {
 
 
     //상속을 이용한 리팩터링
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
-        Payment payment1 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println(payment1);
-        TimeUnit.SECONDS.sleep(1);
-
-        Payment payment2 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-
-        System.out.println(payment2);
-        TimeUnit.SECONDS.sleep(2);
-        Payment payment3 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println(payment3);
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println(payment);
     }
 
 }
