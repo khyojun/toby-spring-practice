31c8c20069db9e8f2c42f7f8ebc2f038ed398b9b
Khyojun <38220795+khyojun@users.noreply.github.com>
2024-07-03 20:48:38 +0900

    first init

---
+++ src/main/java/tobyspring/practicetobyspring/PracticeTobyspringApplication.java
@@ -0,0 +1,13 @@
+package tobyspring.practicetobyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeTobyspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeTobyspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tobyspring/practicetobyspring/PracticeTobyspringApplicationTests.java
@@ -0,0 +1,13 @@
+package tobyspring.practicetobyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeTobyspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
