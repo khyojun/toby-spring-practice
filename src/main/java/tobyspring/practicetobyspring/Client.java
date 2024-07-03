7d23d60942a8638b1150bff5dfe95a0b65f60b50
Khyojun <38220795+khyojun@users.noreply.github.com>
2024-07-03 21:30:02 +0900

    spring 을 활용해서 변경!

--- src/main/java/tobyspring/practicetobyspring/Client.java
+++ src/main/java/tobyspring/practicetobyspring/Client.java
@@ -2,14 +2,17 @@ package tobyspring.practicetobyspring;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Client {
 
 
     //상속을 이용한 리팩터링
     public static void main(String[] args) throws IOException {
-        ObjectFactory objectFactory = new ObjectFactory();
-        PaymentService paymentService = objectFactory.paymentService();
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
+
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
--- src/main/java/tobyspring/practicetobyspring/ObjectFactory.java
+++ src/main/java/tobyspring/practicetobyspring/ObjectFactory.java
@@ -1,11 +1,18 @@
 package tobyspring.practicetobyspring;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ObjectFactory {
 
+    @Bean
     public PaymentService paymentService(){
         return new PaymentService(getExProvider());
     }
 
+    @Bean
     public ExRateProvider getExProvider(){
         return new SimpleExRateProvider();
     }
